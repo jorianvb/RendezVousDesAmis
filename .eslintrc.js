352dfc3f363ab8fc45983110c21f977c06966f84
Valérian Baillet <valerianbaillet@gmail.com>
2025-09-21 19:18:25 +0200

    Ajout des precommit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: [
+    'expo',
+    '@react-native-community',
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended', // Doit être en dernier
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Règles personnalisées
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react-native/no-unused-styles': 'error',
+    'react-native/no-inline-styles': 'warn',
+  },
+};
